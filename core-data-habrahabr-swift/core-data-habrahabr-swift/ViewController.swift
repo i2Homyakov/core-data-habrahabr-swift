4fbcdafccc58b155054d51bd8c2f82b3fb2f0e23
Ilya Khomyakov <>
2019-08-11 10:15:29 +0300

    Empty project

---
+++ core-data-habrahabr-swift/core-data-habrahabr-swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  core-data-habrahabr-swift
+//
+//  Created by Max Zasov on 11/08/2019.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
